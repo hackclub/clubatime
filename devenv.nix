aed46ecbcc8d699fb14d473b962e3326822081f7
Suya Singh <suya1671@tuta.io>
2025-12-27 20:56:58 +0200

    dev: add local postgres db to devenv

--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,10 @@
-{ pkgs, lib, config, inputs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
 
 {
   # https://devenv.sh/packages/
@@ -15,5 +21,21 @@
   languages.typescript.enable = true;
 
   languages.nix.enable = true;
+
+  # https://devenv.sh/services/
+  services.postgres = {
+    enable = true;
+    listen_addresses = "localhost";
+    initialDatabases = [
+      # for the love of all things security, please do not expose this default database.
+      {
+        name = "clubatime";
+        user = "clubatime";
+        pass = "clubatime";
+        schema = ./drizzle;
+      }
+    ];
+  };
+
   # See full reference at https://devenv.sh/reference/options/
 }
